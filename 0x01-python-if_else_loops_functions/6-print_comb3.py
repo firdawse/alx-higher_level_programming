209380ec4faafbaaa10e9b6dcdffb4c2c2f12618
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-09 16:53:56 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(0, 90):
     if i == 89:
         print("{}".format(i))
-    elif int(i/10) == i%10 or int(i/10)> i%10:
+    elif int(i / 10) == i % 10 or int(i / 10) > i % 10:
         continue
     else:
         print("{:02d}".format(i), end=", ")
