d539ec009e432fd1e8ba959bd1e8495753a1c801
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-09 14:12:02 -0700

    task10

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -62,28 +62,3 @@ void free_listint(listint_t *head)
     }
 }
 
-/**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *ptr1 = list;
-	listint_t *ptr2 = list;
-
-	if (!list)
-		return (0);
-
-	while (ptr1 && ptr2 && ptr2->next)
-	{
-		ptr1 = ptr1->next;
-		ptr2 = ptr2->next->next;
-		if (ptr1 == ptr2)
-			return (1);
-	}
-
-	return (0);
-}
-
