43acef7d84f4721e9e073326a5eb7f6377370513
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-10 10:52:20 -0700

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add = 0
+    for i, arg in enumerate(args):
+        add += int(arg)
+    print("{}".format(add))
