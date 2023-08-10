10b1a108848171d913ce31550e1e04731110c4f7
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-10 10:49:34 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 import sys
-
 if __name__ == "__main__":
     args = sys.argv[1:]
     num_args = len(args)
-    ending = "s" if num_args != 1 else ""
-
+    if num_args != 1 :
+        ending = "s" 
+    else :
+        ending = ""
     print("{} argument{}".format(num_args, ending), end="")
     if num_args == 0:
         print(".")
     else:
         print(":")
         for i, arg in enumerate(args):
-            print("{}: {}".format(i+1, arg))                      
+            print("{}: {}".format(i+1, arg))
