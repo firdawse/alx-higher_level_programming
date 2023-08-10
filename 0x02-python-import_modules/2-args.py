17d4dc7cb196d1753df144019ced64bbf2b9aaf5
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-10 10:25:33 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+    if num_args != 1 :
+        ending = "s" 
+    else :
+        ending = ""
+
+    print("{} argument{}".format(num_args, ending), end="")
+    if num_args == 0:
+        print(".")
+    else:
+        print(":")
+        for i, arg in enumerate(args):
+            print("{}: {}".format(i+1, arg))
