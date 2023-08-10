840493e7ae4e596c40211ed539207df712e1437e
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-09 17:10:22 -0700

    task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
-def isupper(c):
-    if ord(c) >= 65 and ord(c) <= 90:
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
+    
+def uppercase(s):
+    output = ""
+    for i in range(len(s)):
+        if islower(s[i]):
+            output += chr(ord(s[i]) - (32))
+        else:
+            output += s[i]
+    print("{}".format(output))
