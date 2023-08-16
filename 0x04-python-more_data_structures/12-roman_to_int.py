cbfedb5707ae93b964470ca4775be0397be40dd6
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-16 14:13:07 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,8 @@ def roman_to_int(roman_string):
         'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
         'CD': 400, 'D': 500, 'CM': 900, 'M': 1000
     }
-
+    if not isinstance(roman_string, str):
+        return 0
     result = 0
     i = 0
     while i < len(roman_string):
