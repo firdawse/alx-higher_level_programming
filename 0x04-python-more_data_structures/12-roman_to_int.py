630c18137dbf3c4c680b362f96a9ec4adad6b52f
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-16 14:11:37 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,11 @@ def roman_to_int(roman_string):
     result = 0
     i = 0
     while i < len(roman_string):
-        result += roman_numerals[roman_string[i]]
-        i += 1
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_numerals:
+            result += roman_numerals[roman_string[i:i+2]]
+            i += 2
+        else:
+            result += roman_numerals[roman_string[i]]
+            i += 1
 
     return result
