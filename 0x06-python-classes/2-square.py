86579e18dac85eee189e0c637134d1914ab8b3e6
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-29 14:04:36 +0000

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,10 +8,14 @@ class Square:
 
     __size = 0
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Args:
             self: used to initiate Square instances
             size (int): size of Square
         """
+	if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+	elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
