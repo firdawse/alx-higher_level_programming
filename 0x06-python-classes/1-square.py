d841e70507d7a19af04ae25e8c6ea920f17d97f7
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-29 13:11:43 +0000

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,13 @@
 
 class Square:
     """one attribute"""
+
 	__size = 0
 
     def __init__(self,size):
         """
         Args:
             self: used to initiate Square instance
-			size: private instance attribute
+			size (int): private instance attribute
         """
         self.__size = size
