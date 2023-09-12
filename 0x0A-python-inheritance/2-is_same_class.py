9a47fe44e0669a94b0e0018b7e9a1a1ad9dbdd75
firdawse <firdawse.guerbouzi1@gmail.com>
2023-09-12 01:25:37 +0000

    task3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-    """ returns True if the object is exactly an instance of the specified class ,otherwise False"""
+    """returns True if the object is exactly an instance of class"""
 
     return True if type(obj) == a_class else False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""check Class Type"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if obj is an instance of a_class or not"""
+
+    return isinstance(obj, a_class)
