2e4fe0e81cf64ad33ae3e194bde7c40160dd55a5
firdawse <firdawse.guerbouzi1@gmail.com>
2023-09-12 01:30:32 +0000

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry():
+    """Base Geometry Module"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry():
+    """Base Geometry Module"""
+
+    def area(self):
+        """Not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Verify parameter type"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
