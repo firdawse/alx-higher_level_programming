50acee1a39efce9c3f31ffe4419209c2cbf41e90
firdawse <firdawse.guerbouzi1@gmail.com>
2023-09-12 16:35:07 +0000

    a

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Class that defines a Student"""
+"""Defines a class Student"""
 
 
 class Student:
-    """class that represents a student"""
+    """Represent a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize attributes"""
+        """Initialize a new Student"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrives dictionary representation"""
+        """Retrieves a dictionary representation"""
 
         if (type(attrs) == list and
-                all(type(e) == str for e in attrs)):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, keu)}
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
