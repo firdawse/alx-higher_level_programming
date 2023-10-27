2b562f629b04f295eb67a71e0fa3054dcd8b0891
firdawse <firdawse.guerbouzi1@gmail.com>
2023-10-27 16:51:07 -0700

    tasks

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Drop the database hbtn-0c_0
+DROP database IF EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- to create a table called first_table in the current db
+CREATE table IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));
+
