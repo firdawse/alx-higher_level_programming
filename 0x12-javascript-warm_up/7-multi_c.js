e50c08cccf73d591ec2e8df6663bc25caa262c9d
firdawse <firdawse.guerbouzi1@gmail.com>
2023-11-15 13:12:46 -0800

    task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that prints x times “C is fun”
+// script that prints x times “C is fun”
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
