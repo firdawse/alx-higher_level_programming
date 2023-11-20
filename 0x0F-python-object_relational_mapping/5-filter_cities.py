b4cbac194e16821b252bfdf012c1f2f80de2851d
firdawse <firdawse.guerbouzi1@gmail.com>
2023-11-20 06:30:10 -0800

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database hbtn_0e_4_usa"""
+"""Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa"""
 import MySQLdb
 from sys import argv
 
@@ -7,10 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    print(", ".join(city[0] for city in rows))
     cur.close()
     db.close()
